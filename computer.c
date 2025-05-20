18d3c282986ad9073ff00f5f0632bb45bb10b040
= <=>
2025-05-20 09:04:17 +0100

    redoing what ive done like the third time

--- computer.c
+++ computer.c
@@ -4,9 +4,12 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <windows.h>
 #include "headers/struct.h"
+#include <time.h>
+#include <conio.h>
 
-#define SLEEP(ms) usleep((ms) * 1000)
+#define SLEEP(ms) Sleep(ms); 
 #define MAX_NAME_LENGTH 50
 #define MAX_ADDRESS_LENGTH 100
 #define MAX_ARRESTS 10
@@ -15,6 +18,7 @@
 #define MAX_CF 50
 #define MAX_AR 50
 #define MAX_EV 50
+#define MAX_PASSWORD_LENGTH 64
 
 typedef struct SNTRPH SNTRPH;
 typedef struct Address Address;
@@ -33,6 +37,8 @@ typedef struct LogList LogList;
 typedef struct OfficerList OfficerList;
 typedef struct GBOX GBOX;
 typedef struct GMessage GMessage;
+
+
 
 struct Address
 {
@@ -161,17 +167,224 @@ struct GMessage{
     bool read;
 };
 
+void increment_id(char *id_string) {
+    int length = strlen(id_string);
+    int i;
 
+    // Find the point where the numeric part begins
+    int number_start = 0;
+    while (number_start < length && isalpha(id_string[number_start])) {
+        number_start++;
+    }
+
+    if (number_start == length || number_start == 0) {
+        printf("Invalid ID format. ID should start with 1 or 2 letters followed by digits.\n");
+        return;
+    }
+
+    // Extract numeric part and increment it
+    int number = atoi(id_string + number_start);
+    number++;
+
+    // Rebuild the ID: Prefix + Incremented number
+    snprintf(id_string + number_start, length - number_start + 1, "%0*ld", length - number_start, number);
+}
+void get_current_date(char *date_buffer) {
+    // Get current time
+    time_t t = time(NULL);
+    
+    // Check if time is valid
+    if (t == (time_t)(-1)) {
+        printf("Failed to get the current time\n");
+        return;
+    }
+
+    // Convert time_t to struct tm (local time)
+    struct tm *tm_info = localtime(&t);
+    
+    // Check if localtime failed
+    if (tm_info == NULL) {
+        printf("Failed to convert time\n");
+        return;
+    }
+
+    // Format the date into the provided buffer (YYYY-MM-DD)
+    strftime(date_buffer, 11, "%Y-%m-%d", tm_info);  // Buffer should be at least 11 characters
+}
+
+void addLog(SNTRPH *sntrph, char *type, char *r_id, Officer *officer, char *desc){
+    AuditLog *log = malloc(sizeof(AuditLog));
+    char audit_id[10];
+    if(sntrph->logList.log_count == 0){
+        strcpy(audit_id, "A000");
+        strcpy(log->audit_id, audit_id);
+    }
+    else{
+        strcpy(audit_id, sntrph->logList.logs[sntrph->logList.log_count-1]->audit_id);
+        increment_id(audit_id);
+        strcpy(log->audit_id, audit_id);
+        
+    }
+    char date[15];
+    get_current_date(date);
+    strcpy(log->timestamp, date);
+    strcpy(log->actionType, type);
+    strcpy(log->record_id, r_id);
+    log->officer = officer;
+    strcpy(log->action_description, desc);
+
+    sntrph->logList.logs[sntrph->logList.log_count++] = log;
+
+}
 void PrintLine(char *text){
     printf("%s\n", text);
     SLEEP(800);
 }
 void startMenu(SNTRPH *sntrph);
 
+void addEvidence(SNTRPH *sntrph){
+    char name[50];
+    char status[20];
+    char type[50];
+    char case_id[10];
+    char desc[500];
+    char file_path[50];
+    char date[20];
+    char custody_chain[5][50];
+    bool is_restricted;
+    char notes[100];
+    Evidence *ev = malloc(sizeof(Evidence));
+
+    get_current_date(date);
+    char id[10];
+    if(sntrph->evidenceArchive.total_items == 0){
+        strcpy(id, "E000");
+        strcpy(ev->evidence_id, id);
+    }else{
+        strcpy(id, sntrph->evidenceArchive.evidence_items[sntrph->evidenceArchive.total_items-1]->evidence_id);
+        increment_id(id);
+        strcpy(ev->evidence_id, id);
+
+    }
+    PrintLine("> Enter the name of your evidence: ");
+    fgets(name, sizeof(name), stdin);
+    PrintLine("> Enter the status of your evidence: ");
+    fgets(status, sizeof(status), stdin);
+    PrintLine("> Enter the type of your evidence: ");
+    fgets(type, sizeof(type), stdin);
+    PrintLine("> Enter the case ID of your evidence: ");
+    fgets(case_id, sizeof(case_id), stdin);
+    PrintLine("> Enter the description of your evidence: ");
+    fgets(desc, sizeof(desc), stdin);
+    PrintLine("> Enter the file path of your evidence: ");
+    fgets(file_path, sizeof(file_path), stdin);
+    PrintLine("> Enter any notes on your evidence: ");
+    fgets(notes, sizeof(notes), stdin);
+    for(int i = 0; i < 5; i++){
+        char buffer[50];
+        PrintLine("> Enter an officer ID that handled this evidence: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+
+        if(strcmp(buffer, "") == 0){
+            PrintLine("Exiting...");
+            break;
+        }
+        strcpy(custody_chain[i], buffer);
+    }
+    ev->is_restricted = false;
+    strcpy(ev->name, name);
+    strcpy(ev->status, status);
+    strcpy(ev->type, type);
+    strcpy(ev->case_id, case_id);
+    strcpy(ev->description, desc);
+    strcpy(ev->file_path, file_path);
+    strcpy(ev->notes, notes);
+    strcpy(ev->date, date);
+    sntrph->evidenceArchive.evidence_items[sntrph->evidenceArchive.total_items++] = ev;
+
+    char officer_name[120];
+    snprintf(officer_name, sizeof(officer_name), "%s %s %s", sntrph->current_user->title, sntrph->current_user->first_name, sntrph->current_user->last_name);
+    char description[200];
+    snprintf(description, sizeof(description), "%s ADDED EVIDENCE: %s ON %s", officer_name, ev->evidence_id, ev->date);
+
+    addLog(sntrph, "EVIDENCE ADDITION", ev->case_id, sntrph->current_user, description);
+}
+void deleteEvidence(SNTRPH *sntrph, const char *evidence_id_to_delete) {
+    bool found = false;
+    char date[15];
+    get_current_date(date);
+    // Loop through the evidence archive to find the matching evidence ID
+    for (int i = 0; i < sntrph->evidenceArchive.total_items; i++) {
+        // If we find a match
+        char officer_name[120];
+        snprintf(officer_name, sizeof(officer_name), "%s %s %s", sntrph->current_user->title, sntrph->current_user->first_name, sntrph->current_user->last_name);
+        if (strcmp(sntrph->evidenceArchive.evidence_items[i]->evidence_id, evidence_id_to_delete) == 0) {
+            found = true;
+            if(sntrph->current_user->clearance < 3){
+                PrintLine("ACCESS DENIED: INSUFFICIENT CLEARANCE");
+                PrintLine("ATTEMPT FOLLOWED IN AUDIT TRAIL");
+
+            char description[200];
+            snprintf(description, sizeof(description), "%s ATTEMPTED TO DELETE EVIDENCE: %s ON %s", officer_name, sntrph->evidenceArchive.evidence_items[i]->evidence_id, date);
+                addLog(sntrph, "EVIDENCE DELETION ATTEMPT", sntrph->evidenceArchive.evidence_items[i]->evidence_id, sntrph->current_user, description);
+            }
+                // Print confirmation message
+            printf("> Evidence with ID %s found. Deleting it...\n", evidence_id_to_delete);
+
+            // Shift all evidence items after the deleted item to the left
+            for (int j = i; j < sntrph->evidenceArchive.total_items - 1; j++) {
+                sntrph->evidenceArchive.evidence_items[j] = sntrph->evidenceArchive.evidence_items[j + 1];
+            }
+
+            char description[200];
+            snprintf(description, sizeof(description), "%s DELETED EVIDENCE: %s ON %s", officer_name, sntrph->evidenceArchive.evidence_items[i]->evidence_id, date);
+            // Decrease the total item count
+            sntrph->evidenceArchive.total_items--;
+            addLog(sntrph, "EVIDENCE DELETION", sntrph->evidenceArchive.evidence_items[i]->evidence_id, sntrph->current_user, description);
+            // Confirm the deletion
+            break;
+        }
+    }
+
+    // If no matching evidence was found
+    if (!found) {
+        PrintLine("> No evidence found with the given ID.");
+    }
+}
+
+void auditTrail(LogList *logList) {
+    for (int i = 0; i < logList->log_count; i++) {
+        AuditLog *log = logList->logs[i];
+
+        // Create officer name line
+        char officer_name[120];
+            snprintf(officer_name, sizeof(officer_name), "%s %s %s",
+                     log->officer->title,
+                     log->officer->first_name,
+                     log->officer->last_name);
+
+        printf("--------------------------------------------------\n");
+        printf("           SAINT-RAPHAEL AUDIT TRAIL IMPRINT\n");
+        printf("--------------------------------------------------\n\n");
+
+        printf("FROM:        %s\n", officer_name);
+        printf("ACTION TYPE: %s\n", log->actionType);
+        printf("RECORD ID:   %s\n", log->record_id);
+        printf("DATE:        %s\n", log->timestamp);
+
+        printf("\n--------------------------------------------------\n");
+        printf("%s\n\n", log->action_description);
+        printf("--------------------------------------------------\n");
+        printf("[Press Enter to view next log]\n");
+
+        getchar(); // Wait for user input to proceed to the next log
+    }
+}
 void MailboxUpdate();
 
 void startMenu(SNTRPH *sntrph) {
-    system("clear"); // Clear screen on macOS
+    system("cls"); // Clear screen on macOS
 
     PrintLine("--------------------------------------------------");
     PrintLine("  SAINT. RAPHAEL DATABASE SYSTEM v1.0\n");
@@ -198,7 +411,9 @@ void startMenu(SNTRPH *sntrph) {
         fgets(username, sizeof(username), stdin);
         username[strcspn(username, "\n")] = 0;
 
-        char* password = getpass("Password: ");
+        char password[64];
+        printf("Password: ");
+        fgets(password, sizeof(password), stdin);
         password[strcspn(password, "\n")] = 0;
         PrintLine("[✓] Credentials received.");
         bool found_user = false;
@@ -429,6 +644,25 @@ void processCommand(SNTRPH *sntrph, char *input){
                     }
                 }
     // sendMessage(sntrph->current_user, , &sntrph);
+    else if(strcmp(token, "ADD") == 0){
+        char *token_2  = strtok(NULL, delimiters);
+        if(strcmp(token_2, "EVIDENCE") == 0){
+            addEvidence(sntrph);
+        }
+    }
+    else if(strcmp(token, "DELETE") == 0){
+        char *token_2  = strtok(NULL, delimiters);
+        if(strcmp(token_2, "EVIDENCE") == 0){
+        char *token_3  = strtok(NULL, delimiters);
+            deleteEvidence(sntrph, token_3);
+        }
+    }
+    else if(strcmp(token, "AUDIT") == 0){
+        char *token_2  = strtok(NULL, delimiters);
+        if(strcmp(token_2, "TRAIL") == 0){
+            auditTrail(&sntrph->logList);
+        }
+    }
 }
 
 void initSNTRPH(SNTRPH *sntrph) {
